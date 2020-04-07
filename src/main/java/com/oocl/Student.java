f47d2459d6f9d23eed6b6e014af0b2e75e831059
Kit Yan <kit.yan@oocl.com>
2020-04-07 14:59:50 +0800

    Finish Q4 Student

--- src/main/java/com/oocl/Student.java
+++ src/main/java/com/oocl/Student.java
@@ -1,18 +1,20 @@
 package com.oocl;
 
 public class Student extends Person{
+    int classNo;
     public Student()
     {
         super();
     }
-    public Student(String name, int age)
+    public Student(String name, int age, int classNo)
     {
         super(name, age);
+        this.classNo = classNo;
     }
 
     @Override
     public String introduce()
     {
-        return String.format("%s Coding for the glory of OOCL.", super.introduce());
+        return String.format("%s I am a student of class %d. Coding for the glory of OOCL.", super.introduce(), classNo);
     }
 }
--- src/test/java/com/oocl/StudentTest.java
+++ src/test/java/com/oocl/StudentTest.java
@@ -9,7 +9,7 @@ public class StudentTest {
     @Test
     public void should_student_introduce_correct()
     {
-        Student student = new Student("Tom", 18);
-        Assert.assertEquals("My name is Tom. I am 18 years old. Coding for the glory of OOCL.", student.introduce());
+        Student student = new Student("Tom", 18, 2);
+        Assert.assertEquals("My name is Tom. I am 18 years old. I am a student of class 2. Coding for the glory of OOCL.", student.introduce());
     }
 }
